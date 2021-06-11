ea6966367a4d6ccede67af694f32601accdc8363
yusokk <13chakbo@naver.com>
2021-06-11 15:51:47 +0900

    trim HomeComponent

--- Home_page/src/pages/home/index.tsx
+++ Home_page/src/pages/home/index.tsx
@@ -1,25 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { RoomListComponent } from './components/room-list';
 
 export function HomeComponent() {
   return (
-    <div className='container'>
-      <div className='lecture-container w-screen'>
-        <div className='titleAndButton'>
-          <div className='titleContainer pt-20'>
-            <div className='title pb- text-center text-3xl font-bold text-blue-400'>
-              언제, 어디서든 그림을 배워봅시다
-            </div>
-            <div>
-              <p className='pt-3 text-center text-2xl font-normal text-gray-400 pb-12'>
-                선생님이 쉽게 알려주실거에요. 걱정은 넣어둬~~
-              </p>
-            </div>
-          </div>
+    <div className='lecture-container w-screen'>
+      <div className='introduction-container pt-24'>
+        <div className='introduction text-center text-4xl font-bold text-blue-400'>
+          언제, 어디서든 그림을 배울 수 있어요
         </div>
-        <div className='mx-52'>
-          <RoomListComponent></RoomListComponent>
+        <div>
+          <p className='pt-3 text-center text-2xl font-normal text-gray-400 pb-16'>
+            선생님이 쉽고 재미있게 알려주실거에요
+          </p>
         </div>
+      </div>
+      <div className='mx-52'>
+        <RoomListComponent></RoomListComponent>
       </div>
     </div>
   );
